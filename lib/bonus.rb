dc620fb8f4fa9d9e078c819ed970f5fec77c9559
Adjoa Ntoso <adjoaan@gmail.com>
2019-06-25 15:53:08 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,12 +20,8 @@ def bonus
    }
   }
 
-  #code your solution here:
+  def epic_tragedy
 
-
+  end
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-
-def epic_tragedy
-
-end
